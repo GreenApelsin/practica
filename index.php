78926a4ada73519034881ffd79fc2bf7286ee68d
GreenApelsin <57299541+GreenApelsin@users.noreply.github.com>
2020-06-22 18:13:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,13 @@
 <head>
 	<meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/login.css">
 	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
 	<title>Авторизация</title>
 </head>
 <body>
 	<div class="container">
-		<h1>User Login</h1>
+		<div class="login-header">User Login</div>
 	</div>
 </body>
 </html>
