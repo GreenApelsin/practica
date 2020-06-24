120bc9f63d220eea0a7433ed24da65fe41add6f9
GreenApelsin <57299541+GreenApelsin@users.noreply.github.com>
2020-06-24 11:19:30 +0300

    Update main.php

--- main.php
+++ main.php
@@ -6,7 +6,21 @@ if(!isset($_SESSION['logged_user'])){
 	header("Location: /");
 }
 
-echo "ok";
+echo '<a href="/logout.php">Выход</a>';
 
 ?>
-<a href="/logout.php">Выход</a>
+
+<html>
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+	<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet">
+	<title>Главная</title>
+</head>
+<body>
+	<div class="container">
+
+	</div>
+</body>
+</html>
