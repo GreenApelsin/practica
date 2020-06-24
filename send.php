d37203b0b1d54b7cfebb07974b4e97b223809d01
GreenApelsin <57299541+GreenApelsin@users.noreply.github.com>
2020-06-24 13:35:07 +0300

    Update send.php

--- send.php
+++ send.php
@@ -32,13 +32,25 @@ if(!isset($_SESSION['logged_user'])){
 			<h1>Send file</h1>
 		<form class="container" action="index.php" method="post">
 	  		<?php 
-	  		if(empty($errors)){
+	  		if(!empty($errors)){
 	  			echo '<div class="errorlogin">'.array_shift($errors).'</div>';
 	  		}
 	  		?>
 	  		<input type="text" name="name" placeholder="Name file" spellcheck="false">
-	  		<input type="password" name="password" placeholder="Password">
-	  		<input type="submit" name="signin" value="Sign in">
+	  		<input type="file" name="file">
+
+	  		<?php 
+	  		$mysql = new mysqli($configs['localhost'], $configs['username'], $configs['password'], $configs['dbname']);
+	  		$cursor = $mysql->query("SELECT `login` FROM `user`;");
+			echo '<select name="select"><option selected="selected">Selected user</option>';
+			while( $result = $cursor->fetch_assoc() ) { 
+        		echo '<option>'.$result['login'].'</option>';
+        	}
+      		echo '</select>';
+	  		$mysql->close();
+	  		?>
+	  		
+	  		<input type="submit" name="send" value="Send">
 	  		<a href="#" style="visibility: hidden;">Forgot Password?</a>
 	  		<a href="/register.php" style="text-align: right;">Create account</a>
 		</form>
