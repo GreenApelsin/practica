a9d38aa7e09cad6a7ac964cd60869802fe1cf1fd
GreenApelsin <57299541+GreenApelsin@users.noreply.github.com>
2020-06-24 21:07:00 +0300

    Update send.php

--- send.php
+++ send.php
@@ -62,9 +62,14 @@ if(!isset($_SESSION['logged_user'])){
         		var i = str.lastIndexOf('\\')+1;
     		}
     		else{
-        		var i = str.lastIndexOf('/')+1;
+    				var i = str.lastIndexOf('/')+1;
     		}						
-    		var filename = str.slice(i);			
+    		
+    		if (i == 0){
+    			var filename = "Выбери или перетащи";
+    		}else{
+    			var filename = str.slice(i);
+    		}			
     		var uploaded = document.getElementById("fileformlabel");
     		uploaded.innerHTML = filename;
 		}
