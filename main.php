5dc1c77bdf8395bb21ee6768d6485e47c88768ae
GreenApelsin <57299541+GreenApelsin@users.noreply.github.com>
2020-06-25 20:05:07 +0300

    Update main.php

--- main.php
+++ main.php
@@ -46,7 +46,7 @@ if(!isset($_SESSION['logged_user'])){
 					}else{
 						echo '<tr class="tr2"><td>';
 					}
-        			echo $result['login'].'</td><td><a href="/file/'.$result['name'].'?forcedownload=1">'.$result['real-name'].'</a></td></tr>';
+        			echo $result['login'].'</td><td><a href="/save.php?f='.$result['name'].'">'.$result['real-name'].'</a></td></tr>';
         			$flagg = !$flagg;
         		}
 				$mysql->close();
