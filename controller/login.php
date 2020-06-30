ff29697768bb74e5ecbffce9f6fc187e51900c78
GreenApelsin <57299541+GreenApelsin@users.noreply.github.com>
2020-06-30 16:12:44 +0300

    Update login.php

--- controller/login.php
+++ controller/login.php
@@ -3,8 +3,8 @@
 // подключаем model авторизации
 include_once "model/auth.php";
 
-// перееадресовываем post в model
-$errors =authLogin($_POST);
+// перееадресовываем post в model, и возвращаем ошибки
+$errors = authLogin($_POST);
 
 // подключаем страницу с автооризацией (внешний вид)
 include_once "view/login.php";
