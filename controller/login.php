fe32048c6a9c91087b91aac23754ba3bf4221a33
GreenApelsin <57299541+GreenApelsin@users.noreply.github.com>
2020-06-30 14:32:31 +0300

    Update login.php

--- controller/login.php
+++ controller/login.php
@@ -1,10 +1,10 @@
 <?php
 echo "открыт контроллер логин\n";
 // подключаем модель
-require "../model/auth.php";
+include_once "model/auth.php";
 
 // перееадресовываем post в модель, в ответ получаем ошибки
 $errors = authLogin($_POST);
 echo "200";
 // подключаем страницу с автооризацией (внешний вид)
-include_once "../view/login.php";
+include_once "view/login.php";
